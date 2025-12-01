7f21ef06c0e2d1f2df491a4e6e0c0e15e503b617
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-12-01 15:24:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+#1-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[1:11]
+print(toza)
+
+#2-misol
+s = "PYTHONCODE"
+print(s)
+
+toza = s[0:-6:2]
+print(toza)
+
+#3-misol
+s = "abracadabra"
+print(s)
+
+toza = s[1:-7]
+print(slice)
+
+#4-misol
+s = "helloslice"
+print(s)
+
+toza = s[2:7]
+print(slice)
+
+#5-misol
+s = "MissionComplete"
+print(s)
+
+toza = s[-8]
+print(toza)
+
+#6-misol
+s = "9876543210"
+print(s)
+
+toza = s[0:7]
+print(slice)
+
+#7-misol
+s = "dictionary"
+print(s)
+
+toza = s[3:-3]
+print(toza)
+
+#8-misol
+s = "PowerfulString"
+print(s)
+
+toza = s[0:-9]
+print(toza)
+
+#9-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[-9:-2]
+print(toza)
+
+#10-misol
+s = "JKLMNOPQRST"
+print(s)
+
+toza = s[-10:-2]
+print(toza)
